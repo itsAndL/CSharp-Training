13715289387346da468d99701dc1c80ff0233b3e
itsAndL <105809250+itsAndL@users.noreply.github.com>
2022-12-17 19:31:46 +0100

    Create Employee.cs

---
+++ WinForms Project Type/MicrosoftEmployeesLibrary/Employee.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftEmployeesLibrary
+{
+    public class Employee
+    {
+        public string FullName { get; set; }
+        public string Department { get; set; }
+        public string Job { get; set; }
+        public string Email { get; set; }
+    }
+}
