27e54f58bca6c17c391b68593175354efd950110
itsAndL <105809250+itsAndL@users.noreply.github.com>
2022-12-28 09:50:00 +0100

    Create Enums.cs

---
+++ WPF Project Type/CarsInOurAgencyStock/AppLibrary/Enums.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLibrary
+{
+    public enum CarBrand
+    {
+        Toyota, Honda, Chevrolet, Ford, Mercedes_Benz,
+        Jeep, BMW, Porsche, Subaru, Nissan, Cadillac,
+        Volkswagen, Lexus, Audi, Ferrari, Volvo, Jaguar,
+        GMC, Buick, Acura, Bentley, Dodge, Hyundai,
+        Lincoln, Mazda, Land_Rover, Tesla, Ram_Trucks,
+        Kia, Chrysler
+    }
+
+    public enum CarCondition { New, Used, Certified }
+}
