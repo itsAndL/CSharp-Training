02557e64c0cbf1c938006c28ce8f9852a79a5235
itsAndL <105809250+itsAndL@users.noreply.github.com>
2022-12-28 10:13:53 +0100

    Create SellYourCar.cshtml.cs

---
+++ BuyAndSellACarUI/Pages/SellYourCar.cshtml.cs
@@ -0,0 +1,25 @@
+using BuyAndSellACarLibrary.DataAccess;
+using BuyAndSellACarLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BuyAndSellACarUI.Pages
+{
+    public class SellYourCarModel : PageModel
+    {
+        [BindProperty]
+        public Car NewCar { get; set; }
+
+        public void OnGet()
+        {
+            
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid == false) return Page();
+            AddNewCar.Add(NewCar);
+            return RedirectToPage("/Index");
+        }
+    }
+}
