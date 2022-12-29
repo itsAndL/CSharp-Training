10ed142ebe30806622b43830cff0a38fd57faef8
itsAndL <105809250+itsAndL@users.noreply.github.com>
2022-12-29 12:15:21 +0100

    Create Seller.cs

---
+++ ASP.NET Core Razor Pages Project Type/BuyAndSellACarLibrary/Models/Seller.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyAndSellACarLibrary.Models
+{
+    public class Seller
+    {
+        public string? FullName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Adress { get; set; }
+        public string? SellerNote { get; set; }
+    }
+}
