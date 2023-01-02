844f9d517d6f9e2ae15cea54d18938e46f11689d
itsAndL <105809250+itsAndL@users.noreply.github.com>
2023-01-02 17:21:48 +0100

    Create TechCompanyModel.cs

---
+++ ASP.NET Core Web API Project Type/Models/TechCompanyModel.cs
@@ -0,0 +1,12 @@
+namespace AspNetCoreWebApiProjectType.Models
+{
+    public class TechCompanyModel
+    {
+        public string ImageSource { get; set; }
+        public string CorporationName { get; set; }
+        public string CustomerService { get; set; }
+        public string Founders { get; set; }
+        public string Headquarters { get; set; }
+        public string Founded { get; set; }
+    }
+}
