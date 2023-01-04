0e6155c0c1b919637a2508db6fe29ef3c361d044
itsAndL <105809250+itsAndL@users.noreply.github.com>
2023-01-04 15:23:14 +0100

    Create CompanyModel.cs

---
+++ Blazor Server Project Type/TechCompaniesLibrary/Models/CompanyModel.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechCompaniesLibrary.Models
+{
+    public class CompanyModel
+    {
+        public string? CompanyName { get; set; }
+        public string? Description { get; set; }
+        public string? Headquarters { get; set; }
+        public string? Founders { get; set; }
+        public string? Founded { get; set; }
+        public string? Url { get; set; }
+        public string? ImageSource { get; set; }
+    }
+}
