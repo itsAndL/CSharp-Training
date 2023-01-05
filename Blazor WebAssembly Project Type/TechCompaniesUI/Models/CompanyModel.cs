ce3e64971417212861f6f7a4bd101f0660f49b35
itsAndL <105809250+itsAndL@users.noreply.github.com>
2023-01-05 10:15:54 +0100

    Create CompanyModel.cs

---
+++ Blazor WebAssembly Project Type/TechCompaniesUI/Models/CompanyModel.cs
@@ -0,0 +1,13 @@
+namespace TechCompaniesUI.Models
+{
+    public class CompanyModel
+    {
+        public string? CompanyName { get; set; }
+        public string? Description { get; set; }
+        public string? Headquarters { get; set; }
+        public string? Founders { get; set; }
+        public string? Founded { get; set; }
+        public string? Url { get; set; }
+        public string? ImageSource { get; set; }
+    }
+}
