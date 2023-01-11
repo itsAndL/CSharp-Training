2d7428016bb9b72e672b234b368f77db9df1032b
itsAndL <105809250+itsAndL@users.noreply.github.com>
2023-01-11 18:42:31 +0100

    Create Profile.cs

---
+++ SQL Server/TRPITUSUserInterface/Models/Profile.cs
@@ -0,0 +1,18 @@
+namespace TRPITUSUserInterface.Models
+{
+    public class Profile
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Residence { get; set; }
+        public string? Citizenship { get; set; }
+        public string? MaritalStatus { get; set; }
+        public int Children { get; set; }
+        public string? Education { get; set; }
+        public string? ImageSource { get; set; }
+        public int Rank { get; set; }
+        public string? NetWorth { get; set; }
+        public string? Source { get; set; }
+        public string? Industry { get; set; }
+    }
+}
