eed6962b5a837f6dd15b0bf7da9181a6381c7e35
itsAndL <105809250+itsAndL@users.noreply.github.com>
2023-01-13 11:05:17 +0100

    Create SQLiteDataAccessService.cs

---
+++ SQLite/TRPITUSLibrary/Services/SQLiteDataAccessService.cs
@@ -0,0 +1,39 @@
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SQLite;
+
+namespace TRPITUSLibrary.Services
+{
+    public class SQLiteDataAccessService
+    {
+        public List<T> GetProfiles<T>(string? sql)
+        {
+            using (IDbConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                return connection.Query<T>(sql).ToList();
+            }
+        }
+
+        public void InsertProfile<T>(string? sql, T model)
+        {
+            using (IDbConnection connection = new SQLiteConnection(LoadConnectionString("Default")))
+            {
+                List<T> profiles = new() { model };
+
+                connection.Execute(sql, profiles);
+            }
+        }
+
+        private string LoadConnectionString(string name = "Default")
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfiguration _configuration = builder.Build();
+
+            return _configuration.GetConnectionString(name);
+            //return System.Configuration.ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
